d27d566c6378a85f56ab43e90e9bccb2948ce639
Tenzout <tpnamgyal221@gmail.com>
2018-03-10 11:49:09 -0600

    Done

--- FindingPrime.py
+++ FindingPrime.py
@@ -8,9 +8,9 @@ number=int(input("Enter a positive number greater than 0: "))
 
 if number>1:
     for i in range(2,number):
-        if (number%1)==0:
+        if (number%i)==0:
             print(number,"is not a prime number")
-            print(i,"times",number/i,"is",number)
+            print(i,"times",number//i,"is",number)
             break
     else:
         print(number,"is a prime number")
